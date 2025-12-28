5a3c5fb6253807d9c88e50e913cc10f598d1372c
Dima Sidenko <sidenkodima2202@gmail.com>
2025-12-28 17:59:17 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const li = document.createElement('li');
+    const ul = document.createElement('ul');
+
+    li.textContent = key;
+    element.append(li);
+
+    if (Object.keys(data[key]).length > 0) {
+      li.append(ul);
+      createTree(ul, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
